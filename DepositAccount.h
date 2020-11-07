5672268e6b1abdf8159d421f7e3d035995d0fdfa
anikachurilova <nyuta.churilova@gmail.com>
2020-11-07 15:34:29 +0200

    added different accounts

--- DepositAccount.h
+++ DepositAccount.h
@@ -1,8 +1,18 @@
 //
 // Created by Anika on 07.11.2020.
 //
-
+#include <string>
+#include <iostream>
+using namespace std;
 #ifndef STARBANKATM_MOOP_KMA_DEPOSITACCOUNT_H
 #define STARBANKATM_MOOP_KMA_DEPOSITACCOUNT_H
 
+class DepositAccount{
+private:
+size_t _depositTerm;
+size_t _depositTerm;
+public:
+
+};
+ostream& operator<<(ostream&, const DepositAccount&);
 #endif //STARBANKATM_MOOP_KMA_DEPOSITACCOUNT_H
