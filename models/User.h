06903b4543639572ec909565311f7175dbb54742
anikachurilova <nyuta.churilova@gmail.com>
2020-11-07 20:10:02 +0200

    user start doing uraaa

--- models/User.h
+++ models/User.h
@@ -1,35 +1,39 @@
-//
-//#include <string>
-//#include <iostream>
-//using namespace std;
-//#include <map>
-//
-//class User {
-//
-//private:
-//    string _firstName;
-//    string _lastName;
-////	map<string,Account> _allAccounts;
-//public:
-//    User(const string&, const string&);
-//    ~User();
-////	void setAccounts(map<string, Account>& _allAccounts);
-//
-//    string getFirstName() const { return _firstName; };
-//    string getLastName() const { return _lastName; };
-////	map<string, Account> getAllAccounts()const { return _allAccounts; };
-//
-//    void seeAllTransactions();
-//    void createTransaction();
-//    void stopAutomatedTransaction();
-//
-//    void putMoneyInAnotherAccount();
-//
-//
-//
-//    void takeCredit();
-//    void openDeposit();
-//    void closeDeposit();
-//};
-//ostream& operator<<(ostream&, const User&);
-//
+
+#include <string>
+#include <iostream>
+using namespace std;
+#include <map>
+
+class User {
+
+private:
+    size_t _id;
+    string _firstName;
+    string _lastName;
+    string _middleName;
+
+public:
+    User(size_t id, string firstName,  string lastName, string middleName);
+    ~User();
+
+
+//	void setAccounts(map<string, Account>& _allAccounts);
+
+    string getFirstName() const { return _firstName; };
+    string getLastName() const { return _lastName; };
+//	map<string, Account> getAllAccounts()const { return _allAccounts; };
+
+    void seeAllTransactions();
+    void createTransaction();
+    void stopAutomatedTransaction();
+
+    void putMoneyInAnotherAccount();
+
+
+
+    void takeCredit();
+    void openDeposit();
+    void closeDeposit();
+};
+ostream& operator<<(ostream&, const User&);
+
