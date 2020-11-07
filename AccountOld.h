6ffc9775c34a19bf4ff4b19b56bd67e93e5aa5c8
valeriia-lun <61375793+valeriia-lun@users.noreply.github.com>
2020-11-07 14:19:27 +0200

    remaking Account and User

--- AccountOld.h
+++ AccountOld.h
@@ -3,33 +3,42 @@
 //
 #include <string>
 #include <iostream>
+#include "User.h"
+
 using namespace std;
 #ifndef STARBANKATM_MOOP_KMA_ACCOUNT_H
 #define STARBANKATM_MOOP_KMA_ACCOUNT_H
 class Account{
 private:
+    size_t _userId; //&||*????
     size_t _cardNumber;
     size_t _pin;
     char* _expiryDate; // maybe just date without time? find how to compare DateTime with Date
     size_t _cvvNumber;
     size_t _sumOnBalance;
+    size_t _limit;
+
 public:
 
     class BadAmount;
     // class BadAccount;
     class BadPIN;
 
+    size_t& userId();
     size_t& cardNumber();
     size_t& pin();
     char& expiryDate();//????
     size_t& cvvNumber();
     size_t& sumOnBalance();
+    size_t& limit();
 
+    const size_t& userId() const;
     const size_t& cardNumber() const;
     const size_t& pin() const;
     const char& expiryDate() const;//????
     const size_t& cvvNumber() const;
     const size_t& sumOnBalance() const;
+    const size_t& lemit() const;
 
     virtual void putMoney();
     void blockCard();
