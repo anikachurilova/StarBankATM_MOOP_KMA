b1ae7fb5202834fde24b80d30977ddaf1e9d59ac
valeriia-lun <61375793+valeriia-lun@users.noreply.github.com>
2020-11-07 21:52:33 +0200

    create ATM.db and created tables

--- main.cpp
+++ main.cpp
@@ -181,22 +181,22 @@ int main(int argc, char **argv)
     }
 
     /* Create SQL statement */
-    sql = "CREATE TABLE COMPANY("  \
-      "ID INT PRIMARY KEY     NOT NULL," \
-      "NAME           TEXT    NOT NULL," \
-      "AGE            INT     NOT NULL," \
-      "ADDRESS        CHAR(50)," \
-      "SALARY         REAL );";
+//    sql = "CREATE TABLE COMPANY("  \
+//      "ID INT PRIMARY KEY     NOT NULL," \
+//      "NAME           TEXT    NOT NULL," \
+//      "AGE            INT     NOT NULL," \
+//      "ADDRESS        CHAR(50)," \
+//      "SALARY         REAL );";
 
     /* Execute SQL statement */
-    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-
-    if( rc != SQLITE_OK ){
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-    } else {
-        fprintf(stdout, "Table created successfully\n");
-    }
+//    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+//
+//    if( rc != SQLITE_OK ){
+//        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+//        sqlite3_free(zErrMsg);
+//    } else {
+//        fprintf(stdout, "Table created successfully\n");
+//    }
     sqlite3_close(db);
         return 0;
 
