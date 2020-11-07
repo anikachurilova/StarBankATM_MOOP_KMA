ba0d7262d782409a257c2a610351a054ef848bce
anikachurilova <nyuta.churilova@gmail.com>
2020-11-07 17:07:29 +0200

    universal account

--- models/accounts/UniversalAccount.cpp
+++ models/accounts/UniversalAccount.cpp
@@ -1,4 +1,19 @@
-//
-// Created by Anika on 07.11.2020.
-//
+#include <time.h>
+#include "UniversalAccount.h"
 
+
+
+UniversalAccount::UniversalAccount(size_t userId, size_t cardNumber, size_t pin, size_t cvv, double sumOnBalance,
+        size_t limit,char* expiryDate,  bool isBlocked):Account(userId,cardNumber,pin,cvv,sumOnBalance,limit,expiryDate,isBlocked){
+
+}
+
+UniversalAccount::~UniversalAccount(){}
+
+void UniversalAccount::putMoney(double amount) {
+       _sumOnBalance += amount;
+}
+
+void UniversalAccount::withdrawMoney(size_t amount){
+    _sumOnBalance -= amount;
+}
--- models/accounts/UniversalAccount.h
+++ models/accounts/UniversalAccount.h
@@ -3,6 +3,10 @@
 #define STARBANKATM_MOOP_KMA_UNIVERSALACCOUNT_H
 class UniversalAccount : public Account {
 public:
+    UniversalAccount(size_t userId, size_t cardNumber, size_t pin, size_t cvv, double sumOnBalance,
+                     size_t limit,char* expiryDate,  bool isBlocked);
+    ~UniversalAccount();
+    void putMoney(double amount) override;
     void withdrawMoney(size_t amount);
 };
 #endif //STARBANKATM_MOOP_KMA_UNIVERSALACCOUNT_H
